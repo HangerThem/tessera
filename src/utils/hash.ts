fe4258e8d07cf93729b0ffae4510be44e8b46c02
HangerThem <f.borisjuk@hangerthem.com>
2026-09-07 15:47:22 +0200

    docs(hash): add docs to hash utility

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,3 +1,9 @@
+/**
+ * Hashes a string using the DJB2 algorithm.
+ * 
+ * @param str The string to hash.
+ * @returns The hash value as a number.
+ */
 export function hashString(str: string): number {
   let h = 0
   for (let i = 0; i < str.length; i++) {
