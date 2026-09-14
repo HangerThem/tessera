01655836e8c55c30454d9ae86c46f9279862c766
HangerThem <f.borisjuk@hangerthem.com>
2026-09-14 08:45:56 +0200

    feat(color): migrate to use isDarkColor in contrastColor

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -5,12 +5,8 @@
  * @returns A contrasting color ('#000' for black or '#fff' for white).
  */
 export function contrastColor(hex: string) {
-	const color = hex.replace('#', '')
-	const r = parseInt(color.slice(0, 2), 16)
-	const g = parseInt(color.slice(2, 4), 16)
-	const b = parseInt(color.slice(4, 6), 16)
-
-	return (r * 299 + g * 587 + b * 114) / 1000 > 128 ? '#000' : '#fff'
+	const isDark = isDarkColor(hex)
+	return isDark ? '#fff' : '#000'
 }
 
 /**
