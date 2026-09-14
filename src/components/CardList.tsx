2fd368d178c5cf312ca19798b21a98ea2f70f267
HangerThem <f.borisjuk@hangerthem.com>
2026-09-14 13:39:40 +0200

    feat(CardList): add sorting by favorite

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'preact/hooks'
 import type { Card } from '../types/Card.type'
 import { CardItem } from './CardItem'
 
 export function CardList({ cards }: { cards: Card[] }) {
+  const [sortedCards, setSortedCards] = useState<Card[]>([])
+
+  useEffect(() => {
+    const sorted = cards.toSorted((a, b) => {
+      if (a.isFavorite && !b.isFavorite) return -1
+      if (!a.isFavorite && b.isFavorite) return 1
+      return a.name.localeCompare(b.name)
+    })
+    setSortedCards(sorted)
+  }, [cards])
+
   return (
     <div class="relative space-y-3 mt-2">
-      {cards.map((card) => (
+      {sortedCards.map((card) => (
         <CardItem key={card.id} card={card} />
       ))}
     </div>
