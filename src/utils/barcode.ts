234a7f7729dbd5b1ec733c012784459c8e77064c
HangerThem <f.borisjuk@hangerthem.com>
2026-09-23 12:00:00 +0200

    feat(barcode): add utitlity to map ZXing format to barcode detector API

--- src/utils/barcode.ts
+++ src/utils/barcode.ts
@@ -35,6 +35,46 @@ export const mapZXingFormatToBWIPJS = (format: BarcodeFormat | QRCodeFormat): st
   }
 }
 
+/**
+ * Maps an array of ZXing barcode formats to the corresponding BarcodeDetector formats.
+ *
+ * @param {Array<BarcodeFormat | QRCodeFormat>} formats - An array of ZXing barcode formats.
+ * @returns {Array<string>} An array of corresponding BarcodeDetector formats.
+ * @throws {Error} If an unsupported barcode format is encountered.
+ */
+export const mapZXingFormatsToBarcodeDetectorFormats = (formats: (BarcodeFormat | QRCodeFormat)[]): BarcodeDetectorOptions["formats"] => {
+  return formats.map((format) => {
+    switch (format) {
+      case BarcodeFormat.CODE_39:
+        return 'code_39'
+      case BarcodeFormat.CODE_93:
+        return 'code_93'
+      case BarcodeFormat.CODE_128:
+        return 'code_128'
+      case BarcodeFormat.EAN_8:
+        return 'ean_8'
+      case BarcodeFormat.EAN_13:
+        return 'ean_13'
+      case BarcodeFormat.ITF:
+        return 'itf'
+      case BarcodeFormat.PDF_417:
+        return 'pdf417'
+      case QRCodeFormat.AZTEC:
+        return 'aztec'
+      case QRCodeFormat.DATA_MATRIX:
+        return 'data_matrix'
+      case QRCodeFormat.QR_CODE:
+        return 'qr_code'
+      case QRCodeFormat.MICRO_QR_CODE:
+        return 'micro_qr_code'
+      case QRCodeFormat.MAXICODE:
+        return 'maxi_code'
+      default:
+        throw new Error(`Unsupported barcode format: ${format}`)
+    }
+  })
+}
+
 /** * Determines if the given format is a QR code format.
  *
  * @param {BarcodeFormat | QRCodeFormat} format - The barcode format to check.
